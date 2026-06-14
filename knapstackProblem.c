1a60c18f22fa2fe1382ac1fac55d1d0c1897bc6b
otaviodev <117609558+otaviofigueredoalves@users.noreply.github.com>
2026-06-14 18:28:42 -0300

    initial commit

---
+++ knapstackProblem.c
@@ -0,0 +1,107 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+typedef struct
+{
+    char nome[50];
+    float peso;
+    int valor;
+    int ativo;
+} Item;
+
+const char *MOCHILA = "mochila.bin";
+
+void cadastrarItens()
+{
+    Item objeto;
+    FILE *arquivo = fopen(MOCHILA, "ab");
+
+    if (arquivo == NULL)
+    {
+        printf("Erro ao abrir o arquivo\n");
+        return;
+    }
+
+    printf("Digite o nome do item:\n");
+    fgets(objeto.nome, 50, stdin);
+    objeto.nome[strcspn(objeto.nome, "\n")] = 0;
+
+    printf("Digite o peso:\n");
+    scanf("%f", &objeto.peso);
+
+    printf("Digite o valor:\n");
+    scanf("%d", &objeto.valor);
+    getchar();
+
+    objeto.ativo = 1;
+
+    fwrite(&objeto, sizeof(Item), 1, arquivo);
+    fclose(arquivo);
+    printf("Item cadastrado!\n");
+}
+
+void exibirItens()
+{
+    Item objeto;
+    FILE *arquivo = fopen(MOCHILA, "rb");
+
+    if(arquivo == NULL){
+        printf("Nenhum item cadastrado.\n");
+        return;
+    }
+
+    printf("@ LISTA DE ITENS\n");
+    while(fread(&objeto, sizeof(Item),1,arquivo)){
+        if(objeto.ativo == 1){
+            printf("NOME: %s\nPESO: %.2f\nVALOR: %d\n",objeto.nome, objeto.peso, objeto.valor);
+        }
+    }
+    fclose(arquivo);
+}
+
+int calcularPeso()
+{
+    Item objeto;
+    float pesoTotal = 0.0;
+    FILE *arquivo = fopen(MOCHILA, "rb");
+
+    if(arquivo == NULL){
+        return 0.0;
+    }
+
+    while(fread(&objeto, sizeof(Item), 1, arquivo)){
+        if(objeto.ativo == 1){
+            pesoTotal += objeto.peso;
+        }
+    }
+    fclose(arquivo);
+    return pesoTotal;
+}
+
+int main()
+{
+    int choose;
+
+    printf("@ ESCOLHA UMA AÇÃO\n");
+    printf("[0] -> CRIAR\n");
+    printf("[1] -> LISTAR\n");
+    printf("[2] -> ATUALIZAR\n");
+    printf("[3] -> DELETE\n");
+    scanf("%d", &choose);
+    getchar();
+
+    switch (choose)
+    {
+        case 0:
+            printf("@ CADASTRAR ESCOLHIDO\n");
+            cadastrarItens();
+            break;
+        case 1:
+            printf("@ LISTAR ITENS\n");
+            exibirItens();
+            break;
+    }
+
+    float pesoMochila = calcularPesoTotal();
+    printf("Peso total é: %.2f kg\n", pesoMochila);
+}
